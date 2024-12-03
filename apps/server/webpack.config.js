c61534e582259c26e262aa9832c31d99e83d3374
nine <yupitereu@hanmail.net>
2024-12-04 06:04:33 +0900

    feat(apps): Project Init

--- apps/server/webpack.config.js
+++ apps/server/webpack.config.js
@@ -1,16 +1,8 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
   resolve: {
     alias: {
       '@': __dirname
     },
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    plugins: [
-      new TsconfigPathsPlugin({
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-        baseUrl: './'
-      })
-    ]
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   }
 };
